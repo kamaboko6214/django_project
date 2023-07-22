72ca4c87e8db29f15bdf03261cb7f0fc75b86f3d
kamaboko <95174703+kamaboko6214@users.noreply.github.com>
2023-07-22 08:41:39 +0000

    add:auth

--- my_project/myapp/myapp/urls.py
+++ my_project/myapp/myapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('index.html', RedirectView.as_view(url='/bbs/')),
     path('bbs/', include('bbs.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
